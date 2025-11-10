c398e4c642dc8d68ab7a345053f4456b8b3c556a
nexthong <hongseungil50@gmail.com>
2025-11-10 15:07:35 +0900

    WorkZones 중앙 정렬 수정: 중복 패딩 제거

--- client/src/pages/WorkZones.tsx
+++ client/src/pages/WorkZones.tsx
@@ -492,7 +492,7 @@ export default function WorkZones() {
 
   return (
     <DashboardLayout>
-      <div className="space-y-6 w-full max-w-[1400px] mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="space-y-6 w-full max-w-[1400px] mx-auto">
         {/* 헤더 */}
         <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between gap-4">
           <div>
